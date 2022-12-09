fcd02d956f308e97c5d9adfc38f7fa00a9eb34cb
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-12-09 17:44:07 -0600

    problem found

--- index.js
+++ index.js
@@ -4,18 +4,24 @@ const cheerio = require('cheerio');
 //get all links from this page where $("div[id='atozindex'] a")
 
 const parent = 'http://catalog.illinois.edu/undergraduate/'
+const node = '.courselistcomment'
+const selectEach = "div[id='atozindex'] a"
+
+
 
 const scrape = async (url) => {
+	console.log(url)
 	try {
 		const { data } = await axios.get(url)
 		const $ = cheerio.load(data)
 		const scrapedData = []
-		$('.courselistcomment').each((_idx, el) => {
+		$(node).each((_idx, el) => {
 			const eachScrape = $(el).text()
 			scrapedData.push(eachScrape)
 		});
+		console.log(scrapedData)
 	} catch (error) {
-		throw error;
+		// throw error;
 	}
 };
 
@@ -27,7 +33,7 @@ const getUrls = async (url) => {
 		const $ = cheerio.load(data)
 
 		let scrapedUrls = []
-		$("div[id='atozindex'] a").each((_idx, el) => {
+		$(selectEach).each((_idx, el) => {
 			const eachScrape = $(el).attr('href')
 			scrapedUrls.push(eachScrape)
 		});
@@ -36,15 +42,17 @@ const getUrls = async (url) => {
 		//some <li> have <ul> those are the ones with concentrations
 		scrapedUrls = scrapedUrls.filter(url => url != '#header')
 		scrapedUrls = scrapedUrls.filter(url => url != undefined)
-		scrapedUrls = scrapedUrls.map(url => `'http://catalog.illinois.edu${url}#degreerequirementstext'`)
-console.log(scrapedUrls)
+		let sliced = scrapedUrls.slice(0,4)
+		// console.log(sliced)
+		scrapedUrls = sliced.map(url => `http://catalog.illinois.edu${url}#degreerequirementstext`)
+// console.log(scrapedUrls)
 
 	scrapedUrls.forEach(element => scrape(element))
 		
 
 		//  return scrapedUrls;
 	} catch (error) {
-		throw error;
+		// throw error;
 	}
 };
 
