59f87e91fdd36cac671d65550aa939931d15d828
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-12-10 13:05:58 -0600

    subQuery

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+
+
 //get all links from this page where $("div[id='atozindex'] a")
 
 const parent = 'http://catalog.illinois.edu/undergraduate/'
 const node = '.courselistcomment'
-const selectEach = "div[id='atozindex'] a"
+const selectEach = "div[id='atozindex'] li"
 
 
 
-const scrape = async (url) => {
-	console.log(url)
-	try {
-		const { data } = await axios.get(url)
-		const $ = cheerio.load(data)
-		const scrapedData = []
-		$(node).each((_idx, el) => {
-			const eachScrape = $(el).text()
-			scrapedData.push(eachScrape)
-		});
-		console.log(scrapedData)
-	} catch (error) {
-		// throw error;
-	}
-};
+// const scrape = async (url) => {
+// 	console.log(url)
+// 	try {
+// 		const { data } = await axios.get(url)
+// 		const $ = cheerio.load(data)
+// 		const scrapedData = []
+// 		$(node).each((_idx, el) => {
+// 			const eachScrape = $(el).text()
+// 			scrapedData.push(eachScrape)
+// 		});
+// 		console.log(scrapedData)
+// 	} catch (error) {
+// 		// throw error;
+// 	}
+// };
 
 
 const getUrls = async (url) => {
+
 	try {
 		//get parent url links
 		const { data } = await axios.get(url)
@@ -34,21 +37,45 @@ const getUrls = async (url) => {
 
 		let scrapedUrls = []
 		$(selectEach).each((_idx, el) => {
-			const eachScrape = $(el).attr('href')
+			//getting the nested majors
+			if (el.children.length > 1) {
+				let liChildren = el.children
+				const subUl = liChildren.find((child) => {
+					return child.name === 'ul'
+				})
+				$(subUl).children().each(function () {
+					const li = $(this)
+					li.children().each(function () {
+						const a = $(this)
+						scrapedUrls.push(a.attr('href'))
+					})
+				})
+			}
+			else{
+				const eachScrape = $(el).attr('href')
 			scrapedUrls.push(eachScrape)
+			}
 		});
+
+		scrapedUrls = scrapedUrls.filter(url => url != '#header')
+		scrapedUrls = scrapedUrls.filter(url => url != undefined)
+
+		console.log(scrapedUrls)
 		//build formated array of links
 		//NEED TO FIX FOR DEGREES WITH CONCENTRATIONS
 		//some <li> have <ul> those are the ones with concentrations
-		scrapedUrls = scrapedUrls.filter(url => url != '#header')
-		scrapedUrls = scrapedUrls.filter(url => url != undefined)
-		let sliced = scrapedUrls.slice(0,4)
-		// console.log(sliced)
-		scrapedUrls = sliced.map(url => `http://catalog.illinois.edu${url}#degreerequirementstext`)
-// console.log(scrapedUrls)
 
-	scrapedUrls.forEach(element => scrape(element))
 		
+		// let sliced = scrapedUrls.slice(0, 4)
+		// // console.log(sliced)
+
+		// scrapedUrls = sliced.map(url => `http://catalog.illinois.edu${url}#degreerequirementstext`)
+		// // console.log(scrapedUrls)
+
+		// // scrapedUrls.forEach(element => scrape(element))
+		// for (const url of scrapedUrls) {
+		// 	await scrape(url)
+		// }
 
 		//  return scrapedUrls;
 	} catch (error) {
